120f4ac7058dc7c8fa9150ce8dcb8eb90c1ea610
Hasaker <Arszrc@gmail.com>
2018-08-01 21:24:40 +0800

    app框架、各个Activity间的切换

---
+++ app/src/main/java/com/application/hasaker/order/AddActivity.java
@@ -0,0 +1,13 @@
+package com.application.hasaker.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
---
+++ app/src/main/java/com/application/hasaker/order/Database.java
@@ -0,0 +1,44 @@
+package com.application.hasaker.order;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+public class Database extends SQLiteOpenHelper {
+
+    private static final String CREATE_FOOD = "create table Food ("
+            + "id integer primary key autoincrement, "
+            + "name text)";
+
+    private static final String TO_DO_LIST = "create table Todo ("
+            + "id integer primary key autoincrement, "
+            + "name text, "
+            + "peppery integer, "
+            + "vinegar boolean, "
+            + "balm boolean, "
+            + "amount integer)";
+
+    private Context mContext;
+
+    public Database(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_FOOD);
+        db.execSQL(TO_DO_LIST);
+        Toast.makeText(mContext, "添加成功！", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists Food");
+        db.execSQL("drop table if exists Todo");
+        onCreate(db);
+    }
+}
